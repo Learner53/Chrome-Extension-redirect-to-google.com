cab3da7031bb0ba17f964d50dc7ccc5d2cb04da3
Sushil Mohan <39714503+Learner53@users.noreply.github.com>
2019-12-09 18:53:41 +0530

    Update content.js

--- content.js
+++ content.js
@@ -1,8 +1,8 @@
 var videosMatch = document.querySelector("title").innerHTML;
 var videosMatched = videosMatch.match(/videos/g);
 var metaSelect = document.querySelectorAll("meta");
-console.log(metaSelect[0].getAttribute("name"));
 
+//Checks if webpage have meta tag with attribute name value description
 for (let i = 0; i < metaSelect.length; i++) {
   if (metaSelect[i].hasAttribute("name")) {
     var matchDesciption = metaSelect[i].getAttribute("name");
